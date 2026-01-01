d1d142b9fbb2d4fd8f20baaeaaf9b570a5fe71fa
jiinsss <jiinsss@users.noreply.github.com>
2026-01-01 23:25:56 +0900

    f

--- stock/sql/ck.sql
+++ stock/sql/ck.sql
@@ -1,4 +1,5 @@
-SELECT symbol, date, COUNT(*)
+SELECT symbol, date, COUNT(*) AS cnt
 FROM `myetl-474505.nmoney.stock_raw`
+WHERE date >= DATE_SUB(CURRENT_DATE(), INTERVAL 30 DAY)
 GROUP BY symbol, date
-HAVING COUNT(*) > 1;
+HAVING cnt > 1;
